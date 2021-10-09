3c7f356913abd2eea592395261a957e56759ba28
Bryon Winger <bryonwinger@gmail.com>
2021-10-09 08:46:50 -0500

    initial User tests

--- members/tests.py
+++ members/tests.py
@@ -1,3 +1,51 @@
+# import unittest
+import datetime as dt
+
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+# from django.utils import timezone
 
 # Create your tests here.
+
+from .models import User
+
+some_date = dt.date(1980, 1, 1)
+
+class UserModelTests(TestCase):
+
+    def test_clean_model(self):
+        """
+        Check clean model.
+        """
+        u = User(first_name='Bill', last_name='Nye',
+                 display_name='bill_nye', birth_date=some_date)
+        self.assertEqual(None, u.full_clean())
+
+    def test_required_fields(self):
+        """
+        Check required fields.
+        """
+        u = User(first_name=None, last_name='Nye',
+                 display_name='bill_nye', birth_date=some_date)
+        with self.assertRaisesRegexp(ValidationError, "first_name"):
+            u.full_clean()
+
+        u = User(first_name='Bill', last_name=None,
+                 display_name='bill_nye', birth_date=some_date)
+        with self.assertRaisesRegexp(ValidationError, "last_name"):
+            u.full_clean()
+
+        u = User(first_name='Bill', last_name='Nye',
+                 display_name=None, birth_date=some_date)
+        with self.assertRaisesRegexp(ValidationError, "display_name"):
+            u.full_clean()
+
+        u = User(first_name='Bill', last_name='Nye',
+                 display_name='bill_nye', birth_date=None)
+        with self.assertRaisesRegexp(ValidationError, "birth_date"):
+            u.full_clean()
+
+    # def test_optional_fields(self):
+    #     """
+    #     Check optional fields.
+    #     """
